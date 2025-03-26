32c242b6aae2805b06d6bdd7ef2d27f7b46395f1
Francesco Poli <fp02@mrc-cbu.cam.ac.uk>
2025-03-26 15:54:38 +0000

    compute_smoothing now primarily relies on coordinates values

--- src/gnm/defaults/compute_spatial_smoothing.py
+++ src/gnm/defaults/compute_spatial_smoothing.py
@@ -1,11 +1,14 @@
 import numpy as np
+from scipy.spatial.distance import squareform, pdist
 
-def compute_spatial_smoothing(distance_matrix, sigma=None):
+
+def compute_spatial_smoothing(coordinates, distance_matrix=None, sigma=None):
     """
     Compute a spatial weight matrix W based on the given distance matrix and sigma.
 
     Parameters
     ----------
+    coordinates : x-y-z coordinates of the network nodes
     distance_matrix : numpy.ndarray
         A 2D square array (N x N) of pairwise distances between points.
     sigma : float, optional
@@ -17,12 +20,15 @@ def compute_spatial_smoothing(distance_matrix, sigma=None):
     numpy.ndarray
         A 2D array (N x N)
     """
-    # Ensure distance_matrix is a NumPy array
-    distance_matrix = np.asarray(distance_matrix)
+    if distance_matrix is None:
+        distance_matrix = squareform(pdist(coordinates))
+    else:
+        # Ensure distance_matrix is a NumPy array
+        distance_matrix = np.asarray(distance_matrix)
 
     # If sigma is not specified, use half the standard deviation of the distance matrix
     if sigma is None:
-        sigma = 0.5 * np.std(distance_matrix)
+        sigma = 0.5 * np.std(coordinates)
 
     # Compute the spatial weight matrix
     W = np.exp(-distance_matrix**2 / (2 * sigma**2))
